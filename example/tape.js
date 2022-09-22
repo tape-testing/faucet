4399c978f624e33d0020d347afefcb44ff1eb0da
Jordan Harband <ljharb@gmail.com>
2022-09-21 22:30:19 -0700

    [eslint] add `npm run lint`

--- example/tape.js
+++ example/tape.js
@@ -1,13 +1,17 @@
+'use strict';
+
 var test = require('tape');
 
 test('beep boop', function (t) {
-    t.plan(2);
-    
-    t.equal(1 + 1, 2);
-    setTimeout(function () {
-        t.deepEqual(
-            'ABC'.toLowerCase().split(''),
-            ['a','b','c']
-        );
-    });
+	t.plan(2);
+
+	t.equal(1 + 1, 2);
+	setTimeout(function () {
+		t.deepEqual(
+			'ABC'.toLowerCase().split(''),
+			[
+				'a', 'b', 'c'
+			]
+		);
+	});
 });
